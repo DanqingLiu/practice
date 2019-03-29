6d4f1112f18f0cc431f24cb2b52fa119338707c0
Danqing Liu <dqliu9@tamu.edu>
2019-03-29 11:45:32 -0500

    fix the issue that only check whether left<current<right is not sufficient

--- coding4_5.cpp
+++ coding4_5.cpp
@@ -20,41 +20,32 @@ struct Node* newnode(int data){
   return node; 
 } 
 
-int validateBST(Node *root){
-  if(root->left == NULL and root->right == NULL){
+bool validateBST(Node *root, int min, int max){
+  if(root == NULL){
     return true;
   }
-  else if(root->left == NULL and root->right != NULL){
-    if(root->data < root->right->data){
-      validateBST(root->right);
-    }
-    else{return false;}
+  else if((min != NULL and root->data < min) or (max != NULL and root->data > max)){
+    return false;
   }
-  else if(root->right == NULL and root->left != NULL){
-    if(root->data > root->left->data){
-      validateBST(root->left);
-    }
-    else{return false;}
-  }
-  else if (root->data > root->left->data and root->data < root->right->data){
-    validateBST(root->left);
-    validateBST(root->right);
+  else if(!validateBST(root->left, min, root->data) or !validateBST(root->right, root->data, max)){
+    return false;
   }
   else{
-    return false;
+    return true;
   }
 }
   
-int main() 
-{ 
+int main(){ 
   struct Node *root = newnode(8); 
   root->left        = newnode(6); 
-  root->right       = newnode(2); 
+  root->right       = newnode(12); 
   root->left->left  = newnode(5); 
-  root->left->right = newnode(7); 
+  root->left->right = newnode(9); 
   root->left->left->left  = newnode(3); 
  
-  bool isBST = validateBST(root);
+  int min = NULL;
+  int max = NULL;
+  bool isBST = validateBST(root, min, max);
   cout << "is a binary search tree? " << isBST << endl;
   return 0; 
 } 
