6cf9a3ee07e07fd8d0cd24a64ba9c421979dafd0
Danqing Liu <dqliu9@tamu.edu>
2019-04-01 16:26:48 -0500

    fixed the issue that some nodes has no right subtrees

--- coding4_6.cpp
+++ coding4_6.cpp
@@ -1,5 +1,6 @@
 /* 
 Successor
+https://www.geeksforgeeks.org/binary-search-tree-insert-parent-pointer/
 */
 #include<iostream>
 #include<limits>
@@ -10,6 +11,7 @@ struct Node{
   int data;
   struct Node *left;
   struct Node *right;
+  struct Node *parent;
 };
 
 struct Node* newnode(int data){ 
@@ -17,27 +19,62 @@ struct Node* newnode(int data){
   node->data = data; 
   node->left = NULL; 
   node->right = NULL; 
+  node->parent = NULL; 
   return node; 
 } 
 
-Node *getMin(Node *root){
-  if(root->left == NULL){
-    return root;
+Node *getMin(Node *n){
+  while(n->left != NULL){
+    n = n->left;
   }
+  return n;
+}
+
+Node *findNext(Node *n){
+  if(n == NULL) return NULL;
   else{
-    getMin(root->left);
+    if(n->right != NULL){
+      getMin(n->right);
+    }
+    else{
+      Node *q = n;
+      Node *x = n->parent;
+      while(q != x->left){
+        q = x;
+        x = x->parent;
+      }
+      return x;
+    }
   }
 }
   
+struct Node* insert(struct Node* node, int key){ 
+  if(node == NULL) return newnode(key); 
+  if(key < node->data){ 
+    Node *lchild = insert(node->left, key); 
+    node->left  = lchild; 
+    lchild->parent = node; 
+  } 
+  else if(key > node->data){ 
+    Node *rchild = insert(node->right, key); 
+    node->right  = rchild; 
+    rchild->parent = node; 
+  } 
+  return node; 
+} 
+  
 int main(){ 
-  struct Node *root = newnode(8); 
-  root->left        = newnode(6); 
-  root->right       = newnode(12); 
-  root->left->left  = newnode(5); 
-  root->left->right = newnode(7); 
-  root->left->left->left  = newnode(3); 
- 
-  Node *next = getMin(root->right);
-  cout << "The next node of root: " << next->data << endl;
+  struct Node *root = NULL; 
+  root = insert(root, 20); 
+  insert(root, 15); 
+  insert(root, 30); 
+  insert(root, 14); 
+  insert(root, 18); 
+  insert(root, 16); 
+  insert(root, 25); 
+  Node *n = findNext(root);
+  cout << "The next of " << root->data << " is " << n->data << endl;
+  Node *m = findNext(root->left->right);
+  cout << "The next of " << root->left->right->data << " is " << m->data << endl;
   return 0; 
 } 
